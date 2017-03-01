ab8ca427e8e40c9f6b58fe8a8a4d08121aee7b0d
seductorAmadeus <martinraila@mail.ru>
2017-03-01 17:38:21 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Лабораторная работа №6</title>
+    <style>
+        body {
+            background: #4f4f4f;
+            background-size: cover;
+            margin: 0;
+        }
+
+        /* create two columns of the same width */
+        table {
+            table-layout: fixed;
+            width: 100%;
+        }
+
+        td, th {
+            width: 100%;
+        }
+
+        .header-bg {
+            text-align: justify;
+            height: 10em;
+            padding: 2em 3%;
+            background: #3d0913;
+            color: #fff;
+        }
+
+        .header-font {
+            font-family: cursive;
+            font-size: 18px;
+            color: #c0c1d8;
+        }
+
+        #col1 {
+            background-color: #c0c1d8;
+            height: 35em;
+
+        }
+
+        #col2 {
+
+        }
+    </style>
+</head>
+<body>
+<header>
+    <div class="header-bg">
+        <div class="header-font">
+            <H3>Райла Мартин, гр.P3211, вариант №3010</H3>
+        </div>
+    </div>
+</header>
+<table width="90%" cellpadding="5" cellspacing="0">
+    <tr>
+        <td id="col1">4344</td>
+        <td id="col2">21512</td>
+    </tr>
+</table>
+</body>
+
+</html>
