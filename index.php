10131c2361f2944994f7af91b2d27cfee855f7b5
seductorAmadeus <martinraila@mail.ru>
2017-03-01 19:26:05 +0300

    Add footer, image; refactoring

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
     <title>Лабораторная работа №6</title>
     <style>
         body {
-            background: #4f4f4f;
             background-size: cover;
+            background-color: #3d0913;
             margin: 0;
+        }
+
+        footer {
+
+        }
+
+        p {
+            text-align: center;
         }
 
         /* create two columns of the same width */
@@ -19,12 +27,15 @@
             width: 100%;
         }
 
+        .footer-text {
+            color: #c0c1d8;
+        }
+
         .header-bg {
             text-align: justify;
-            height: 10em;
+            height: 8em;
             padding: 2em 3%;
             background: #3d0913;
-            color: #fff;
         }
 
         .header-font {
@@ -33,18 +44,29 @@
             color: #c0c1d8;
         }
 
-        #col1 {
-            background-color: #c0c1d8;
-            height: 35em;
+        .img {
+            display: block;
+            margin: 0 auto;
+            border: 3px solid #4f4f4f;
+        }
 
+        #col1 {
+            background-color: #4f4f4f;
+            height: 40em;
         }
 
         #col2 {
+            background-color: #c0c1d8;
+        }
 
+        a:link {
+            color: #8685e1;
         }
     </style>
 </head>
+
 <body>
+
 <header>
     <div class="header-bg">
         <div class="header-font">
@@ -52,12 +74,24 @@
         </div>
     </div>
 </header>
+
 <table width="90%" cellpadding="5" cellspacing="0">
     <tr>
-        <td id="col1">4344</td>
-        <td id="col2">21512</td>
+        <td id="col1"></td>
+        <td id="col2">
+            <p><img class="img" src="images/figure.png" alt="Фигура" width="420" height="350"></p>
+        </td>
     </tr>
 </table>
+
+<footer class="footer-text">
+    <p>Posted by: seductorAmadeus</p>
+    <p>Contact information:
+        <a href="mailto:Rayla.Martin.IT@gmail.com">
+            Rayla.Martin.IT@gmail.com</a>
+    </p>
+</footer>
+
 </body>
 
 </html>
